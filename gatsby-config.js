6a6e63efb946b6fc2d89576fd7d1942465b2eab7
Daisho <daishokomiyama@gmail.com>
2021-11-24 13:04:38 -0500

    Add contact form component (prototype)

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,9 @@ module.exports = {
       'https://res.cloudinary.com/de9x7yfyb/image/upload/v1637543915/samples/qc3/profile/2021-profile-bright-opt_alp6vj.jpg',
   },
   plugins: [
-    'gatsby-plugin-root-import', // This enables absolute paths for import (e.g. 'src/components' instead of '../../components')
+    'gatsby-plugin-root-import', // Enables absolute paths for import (e.g. 'src/components' instead of '../../components')
     'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet', // This enables to set HTML head element
+    'gatsby-plugin-react-helmet', // Enables to set HTML head element
 
     // This set of plugins is to enable creating MDX blog posts from the src/posts folder
     {
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+
+export default function ContactForm() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  // TODO: Handle these three values within a single function
+
+  const handleSendMessage = async e => {
+    e.preventDefault()
+    const url = 'http://localhost:3000/send' // TODO: create own service (node + express )
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors', // no-cors, *cors, same-origin
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        }),
+      })
+      console.log('Success sending form', response.json())
+      // TODO: Set a toast message
+    } catch (err) {
+      console.error('Error sending form', err)
+      // TODO: Set a toast message
+    }
+  }
+
+  return (
+    <form>
+      <label htmlFor='name'>Name</label>
+      <input
+        type='text'
+        id='name'
+        name='name'
+        onChange={e => setName(e.target.value)}
+        placeholder='Your name..'
+      />
+
+      <label htmlFor='email'>Email</label>
+      <input
+        type='text'
+        id='email'
+        name='email'
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+        placeholder='Your email'
+      />
+      <label htmlFor='message'>Message</label>
+      <textarea
+        id='message'
+        name='message'
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+        placeholder='Write something..'
+      />
+      <button type='submit' onClick={handleSendMessage}>
+        Submit
+      </button>
+    </form>
+  )
+}
