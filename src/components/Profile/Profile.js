8a471969cae7ee3ebcdc7a9d23a7446200e509d0
Daisho <daishokomiyama@gmail.com>
2021-11-25 17:44:36 -0500

    Add profile section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { profile } from 'src/components/Profile/profile.module.scss'
+import { profile, card, text } from 'src/components/Profile/profile.module.scss'
 
 export default function Profile() {
   return (
-    <div className={`right ${profile}`}>
-      <img
-        src='https://res.cloudinary.com/de9x7yfyb/image/upload/c_scale,e_brightness:10,q_auto,r_0,w_200/v1637735204/qc3/2021-profile-crop-opt_x0qdi1.jpg'
-        alt='Kyoshin'
-      />
-    </div>
+    <aside>
+      <div className={card}>
+        <div className={profile}>
+          <img
+            src='https://res.cloudinary.com/de9x7yfyb/image/upload/c_scale,e_brightness:10,q_auto,r_0,w_200/v1637735204/qc3/2021-profile-crop-opt_x0qdi1.jpg'
+            alt='Kyoshin'
+          />
+        </div>
+        <div className={text}>
+          <p>この人が書いてます。</p>
+          <h2>Kyoshin</h2>
+          <p>
+            2010年よりモントリオール在住のカナダ市民権保持者。
+            元陸自で現在Web屋。 カレー好き。
+          </p>
+        </div>
+      </div>
+    </aside>
   )
 }
