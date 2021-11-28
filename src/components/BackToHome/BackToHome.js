35f25cee06c13169fcfa496f03d137aff5e3e2b2
Daisho <daishokomiyama@gmail.com>
2021-11-27 20:34:50 -0500

    Fix BackToHome button

--- src/components/BackToHome/BackToHome.js
+++ src/components/BackToHome/BackToHome.js
@@ -1,15 +1,18 @@
+import { PRIMARY, YELLOW } from 'src/components/constants'
+import React, { useState } from 'react'
 import ArrowLeft from 'src/images/ArrowLeft'
 import { Link } from 'gatsby'
-import React from 'react'
 import { backToHome } from './backToHome.module.scss'
 
-export default function BackToHome({ onMouseEnter, onMouseLeave, color }) {
+export default function BackToHome() {
+  const [color, setColor] = useState(PRIMARY)
+
   return (
     <Link
       className={backToHome}
       to='/'
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
+      onMouseEnter={() => setColor(YELLOW)}
+      onMouseLeave={() => setColor(PRIMARY)}
     >
       <ArrowLeft color={color} />
       　ホームへ戻る
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -1,19 +1,12 @@
-import { PRIMARY, YELLOW } from 'src/components/constants'
-import React, { useEffect, useState } from 'react'
 import BackToHome from 'src/components/BackToHome'
 import Layout from 'src/components/Layout'
+import React from 'react'
 
 export default function Posts() {
-  const [color, setColor] = useState(PRIMARY)
-
   return (
     <Layout title='投稿記事一覧' description='Quebec3の投稿記事一覧ページ'>
       <h1>投稿記事一覧</h1>
-      <BackToHome
-        color={color}
-        onMouseEnter={() => setColor(YELLOW)}
-        onMouseLeave={() => setColor(PRIMARY)}
-      />
+      <BackToHome />
     </Layout>
   )
 }
