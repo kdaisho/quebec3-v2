1d9dc781208716fe9c9d3ea3620d2d14b7086814
Daisho Komiyama <daishokomiyama@gmail.com>
2021-12-17 00:27:11 -0500

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import React from 'react'
 export default function PageNotFound() {
   return (
     <Layout>
-      <h1>404 Page Not Found</h1>
+      <h1>Umm 404 Page Not Found</h1>
       <p>お探しのページは見つかりませんでした。</p>
       <Link to='/'>ホームページへ戻る</Link>
     </Layout>
