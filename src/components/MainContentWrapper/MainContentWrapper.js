aa24fad2c46d6cc40941f12d4374faafe2499412
Daisho Komiyama <daishokomiyama@gmail.com>
2021-12-27 15:13:33 -0500

    Add style to 404 page

--- src/components/MainContentWrapper/MainContentWrapper.js
+++ src/components/MainContentWrapper/MainContentWrapper.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import cn from 'classnames'
 import { mainContent } from './main-content-wrapper.module.scss'
 
-export default function MainContentWrapper({ children }) {
-  return <main className={mainContent}>{children}</main>
+export default function MainContentWrapper({ classNameProp, children }) {
+  return (
+    <main className={cn(mainContent, { [classNameProp]: classNameProp })}>
+      {children}
+    </main>
+  )
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,14 @@ import BackToHome from 'src/components/BackToHome'
 import Layout from 'src/components/Layout'
 import MainContentWrapper from 'src/components/MainContentWrapper'
 import React from 'react'
+import { is404 } from 'styles/404.module.scss'
 
 export default function PageNotFound() {
   return (
     <Layout>
-      <MainContentWrapper>
-        <h1>Hum, 404 Page Not Found</h1>
-        <p>お探しのページは見つかりませんでした。</p>
+      <MainContentWrapper classNameProp={is404}>
+        <h1>404 Page Not Found</h1>
+        <p>悪いニュースです。お探しのページは見つかりません。</p>
         <BackToHome destination='/' text='ホームへ戻る' />
       </MainContentWrapper>
     </Layout>
