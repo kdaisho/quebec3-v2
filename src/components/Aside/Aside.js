67dfdfb2f301213acb48cce6bf5c70216baf4298
Daisho Komiyama <daishokomiyama@gmail.com>
2021-12-29 13:45:12 -0500

    Update sidebar: aside

--- src/components/Aside/Aside.js
+++ src/components/Aside/Aside.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { card, isNewsletter } from './aside.module.scss'
+import { card, isNewsletter, sidebar } from './aside.module.scss'
 import Profile from 'src/components/Profile'
 import SubscriptionForm from 'src/components/SubscriptionForm'
 
@@ -7,12 +7,12 @@ export default function Aside() {
   const [showSubscription, setShowSubscription] = useState(false)
 
   return (
-    <aside>
+    <aside className={sidebar}>
       <Profile />
       <div className={`${card} ${isNewsletter}`}>
         <h3>記事更新通知サービス</h3>
         <button onClick={() => setShowSubscription(true)}>
-          手続方法について
+          登録方法について
         </button>
       </div>
       {showSubscription && (
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,23 +4,21 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Profile() {
   return (
-    <aside>
-      <div className={card}>
-        <StaticImage
-          src='../../images/profile-circle-opt.png'
-          alt='kyoshin'
-          placeholder='blurred'
-          className={profile}
-        />
-        <div className={text}>
-          <p>性格悪そーなこの人が書いてます。</p>
-          <h2>Kyoshin</h2>
-          <p>
-            2010年よりモントリオール島在住のカナダ市民権保持者。
-            元陸上自衛官で現在はWeb屋。 カレー好き。
-          </p>
-        </div>
+    <div className={card}>
+      <StaticImage
+        src='../../images/profile-circle-opt.png'
+        alt='kyoshin'
+        placeholder='blurred'
+        className={profile}
+      />
+      <div className={text}>
+        <p>性格悪そーなこの人が書いてます。</p>
+        <h2>Kyoshin</h2>
+        <p>
+          2010年よりモントリオール島在住のカナダ市民権保持者。
+          元陸上自衛官で現在はWeb屋。 カレー好き。
+        </p>
       </div>
-    </aside>
+    </div>
   )
 }
