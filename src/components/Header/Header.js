b084b0746fde4fdf34e841918e05912bc15f3db7
Daisho Komiyama <daishokomiyama@gmail.com>
2021-12-29 13:59:37 -0500

    Add backdrop to mobile nav expanded state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
-import { container, hamburger, header, menuButton } from './header.module.scss'
+import {
+  backdrop,
+  container,
+  hamburger,
+  header,
+  menuButton,
+} from './header.module.scss'
 import { LOGO } from 'src/components/constants'
 import { Link } from 'gatsby'
 import Logo from 'src/svg/Logo'
@@ -10,34 +16,42 @@ export default function Header() {
   const [menuExpanded, setMenuExpanded] = useState(false)
 
   return (
-    <header className={header}>
-      <div className={container}>
-        <Link
-          to='/'
-          onMouseEnter={() => setLogoColor(LOGO.HOVERED)}
-          onMouseLeave={() => setLogoColor(LOGO.DEFAULT)}
-        >
-          <Logo
-            outline={logoColor.outline}
-            inner={logoColor.inner}
-            title='logo'
-          />
-        </Link>
-
-        <nav className={cn({ expanded: menuExpanded })}>
-          <Link to='/'>ホーム</Link>
-          <Link to='/blogs/1'>記事一覧</Link>
-          <Link to='/about'>Quebec3とは</Link>
-          <Link to='/contact'>接触を試みる</Link>
-        </nav>
-      </div>
-      <button className={menuButton} onClick={() => setMenuExpanded(ex => !ex)}>
-        <div className={cn({ 'is-active': menuExpanded }, hamburger)}>
-          <div className='hamburger-box'>
-            <div className='hamburger-inner'></div>
-          </div>
+    <>
+      <header className={header}>
+        <div className={container}>
+          <Link
+            to='/'
+            onMouseEnter={() => setLogoColor(LOGO.HOVERED)}
+            onMouseLeave={() => setLogoColor(LOGO.DEFAULT)}
+          >
+            <Logo
+              outline={logoColor.outline}
+              inner={logoColor.inner}
+              title='logo'
+            />
+          </Link>
+          <nav className={cn({ expanded: menuExpanded })}>
+            <Link to='/'>ホーム</Link>
+            <Link to='/blogs/1'>記事一覧</Link>
+            <Link to='/about'>Quebec3とは</Link>
+            <Link to='/contact'>接触を試みる</Link>
+          </nav>
         </div>
-      </button>
-    </header>
+
+        <button
+          className={menuButton}
+          onClick={() => setMenuExpanded(ex => !ex)}
+        >
+          <div className={cn({ 'is-active': menuExpanded }, hamburger)}>
+            <div className='hamburger-box'>
+              <div className='hamburger-inner'></div>
+            </div>
+          </div>
+        </button>
+      </header>
+      {menuExpanded && (
+        <div className={backdrop} onClick={() => setMenuExpanded(false)}></div>
+      )}
+    </>
   )
 }
